ea292367f2c5f0213325ae6646dd92ff23341aa7
shahriaarrr <75499598+shahriaarrr@users.noreply.github.com>
2021-03-23 08:38:51 +0430

    edit hello.py file and create game

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello Shahriar")
+h = True
+
+hi = '''
+hi.welcome to git course's game.Choose your desired game from the following options:
+1.random number
+2.taas
+3.revers number
+4.exit
+ '''
+
+print(hi)
